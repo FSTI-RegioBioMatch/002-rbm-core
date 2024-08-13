03b7865ec97d38fecb900fea14de6d417bd6e419
LukeDerpwalka <lukaskreuzer@outlook.de>
2024-08-13 11:45:50 +0200

    my menu

--- src/main/java/de/regiobiomatch/rbmcore/rest/controllers/NewMenuPlanController.java
+++ src/main/java/de/regiobiomatch/rbmcore/rest/controllers/NewMenuPlanController.java
@@ -8,8 +8,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-import java.util.Optional;
-
 
 @RestController
 @RequestMapping("/api/v1/new-menu-plans")
